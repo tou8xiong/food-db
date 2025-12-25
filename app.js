b174f1385327db4f5dc523f46fbd1d3a36a5e75d
touxy <touxhk@gmail.com>
2025-12-25 09:54:32 +0700

    'check'

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 app.use(cors());
@@ -102,7 +101,8 @@ app.delete('/foodmenu/:id', (req, res) => {
     );
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Server running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 });
 
