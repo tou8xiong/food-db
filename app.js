cdb69fea990b9eca3666efc595b0e804a7117af8
touxy <touxhk@gmail.com>
2025-12-25 10:02:21 +0700

    tt

--- app.js
+++ app.js
@@ -101,8 +101,8 @@ app.delete('/foodmenu/:id', (req, res) => {
     );
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on port ${port}`);
 });
 
